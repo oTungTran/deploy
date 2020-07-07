8d9d20d2372faccfe2964e37dd985b6b86e9ebbc
oTungTran <66772475+oTungTran@users.noreply.github.com>
2020-07-07 08:50:59 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <head>
     <title>Login Page</title>
-    <link rel="stylesheet" type="text/css" href="style1.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 
     <body>
 
@@ -11,9 +11,9 @@
             <h1>Welcome to Toy Shop</h1>
             <form action="connect.php" method="POST" accept-charset="utf-8">
                 <p>Username</p>
-                <input type="text" name="username" placeholder="Enter Username">
+                <input type="text" name="" placeholder="Enter Username">
                 <p>Password</p>
-                <input type="password" name="pass" placeholder="Enter Password">
+                <input type="password" name="" placeholder="Enter Password">
                 <input type="submit" name="" value="Login">
                 <a href="#">Lost your password?</a><br>
                 <a href="#">Don't have an account?</a>
